849f1a15387edf3d19794d56340edb722c47c0a8
daniel <dm.danielmunteanu@gmail.com>
2026-05-02 17:45:42 +0300

    Update

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
-cp -r "${HOME}/.config/niri" "./config"
+cp -r "${HOME}/.config/niri" ./config
 
 # remove whatever is not used
-rm "./config/niri/alttab.kdl"
-rm "./config/niri/layout.kdl"
-rm "./config/niri/outputs.kdl"
-rm "./config/niri/wpblur.kdl"
-rm -r "./config/niri/profiles"
+rm ./config/niri/dms/alttab.kdl
+rm ./config/niri/dms/layout.kdl
+rm ./config/niri/dms/outputs.kdl
+rm ./config/niri/dms/wpblur.kdl
+rm ./config/niri/config.kdl.backup*
+rm -r ./config/niri/dms/profiles
