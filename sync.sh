83c0846f3c74d50b9af7a262f189bd44b0fe13c0
daniel <dm.danielmunteanu@gmail.com>
2026-05-02 21:09:56 +0300

    Update

--- sync.sh
+++ sync.sh
@@ -1,9 +1,13 @@
 cp -r "${HOME}/.config/niri" ./config
+cp -r "${HOME}/.config/kitty" ./config
 
 # remove whatever is not used
 rm ./config/niri/dms/alttab.kdl
 rm ./config/niri/dms/layout.kdl
 rm ./config/niri/dms/outputs.kdl
 rm ./config/niri/dms/wpblur.kdl
-rm ./config/niri/config.kdl.backup*
 rm -r ./config/niri/dms/profiles
+
+# remove backups, no need of them
+rm ./config/niri/config.kdl.backup*
+rm ./config/kitty/kitty.conf.bak*
